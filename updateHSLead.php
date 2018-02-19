942628782c02fa30086d71f380bfc9a8e6592870
Jesper Reinholdt <jesperreinholdt@gmail.com>
2018-02-19 13:58:55 +0100

    1.0

---
+++ updateHSLead.php
@@ -0,0 +1,19 @@
+<?php
+
+        $hapikey = "b1425f46-e489-42fc-aeaf-fcbc716f34c5";
+        $leadID = "505501";
+        $endpoint = 'https://api.hubapi.com/contacts/v1/contact/vid/'.$leadID.'/profile?hapikey='.$hapikey;
+        $ch = @curl_init();
+        @curl_setopt($ch, CURLOPT_POST, true);
+        @curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
+        @curl_setopt($ch, CURLOPT_URL, $endpoint);
+        @curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        @curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = @curl_exec($ch);
+        $status_code = @curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curl_errors = curl_error($ch);
+        @curl_close($ch);
+        echo "curl Errors: " . $curl_errors;
+        echo "\nStatus code: " . $status_code;
+        echo "\nResponse: " . $response;
+
